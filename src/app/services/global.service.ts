962efe9f48354b0f2eb5f533de67ba5d969f0a71
Lockheed-stack <986667243@qq.com>
2024-10-08 16:26:59 +0800

    domain name update

--- src/app/services/global.service.ts
+++ src/app/services/global.service.ts
@@ -4,7 +4,7 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class GlobalService {
-  public domain: string = "http://localhost:8000/";
+  public domain: string = "https://lee-marcus.one//";
   public qiniuImgsURL: string = this.domain + "qiniu-img/";
   public qiniuArticleURL: string = this.domain + "qiniu-article/";
   public imagePlaceholder:string = "https://placehold.co/200x200?text=Image";
