c187fb65a0657c38aa4a53311c87906bf1eb24e6
Lockheed-stack <986667243@qq.com>
2024-10-21 22:07:33 +0800

    update blog display interface && add new footer design

--- src/app/blog-display/blog-display.component.ts
+++ src/app/blog-display/blog-display.component.ts
@@ -12,9 +12,11 @@ import hljs from 'highlight.js/lib/common';
 import hljs_dockerfile from 'highlight.js/lib/languages/dockerfile'
 import { ArticleInfo, ArticlesService } from '../services/articles.service';
 import { ActivatedRoute } from '@angular/router';
-import { map, switchMap } from 'rxjs';
+import {MatCardModule} from '@angular/material/card';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { SnackBarComponent } from '../shared/snack-bar/snack-bar.component';
+import { UserInfo, UserService } from '../services/user.service';
+import { FooterComponent } from '../shared/footer/footer.component';
 
 hljs.registerLanguage('dockerfile', hljs_dockerfile);
 
@@ -27,12 +29,13 @@ hljs.registerLanguage('dockerfile', hljs_dockerfile);
     BlogMarkdownComponent,
     NzSkeletonModule,
     CdkDrag,
-    NgIf,
     MatIcon,
     MatButtonModule,
     CdkListbox,
     CdkOption,
-    NzDrawerModule
+    NzDrawerModule,
+    MatCardModule,
+    FooterComponent,
   ],
   templateUrl: './blog-display.component.html',
   styleUrl: './blog-display.component.scss',
@@ -42,22 +45,25 @@ export class BlogDisplayComponent implements OnInit, OnChanges {
   constructor(
     private articleService: ArticlesService,
     private route: ActivatedRoute,
+    private userService: UserService,
   ) { }
   readonly snackbar = inject(MatSnackBar);
-  loading: boolean = false;
+  loading: boolean = true;
   openSidebar: boolean = false;
   titleArray: Array<Title> = [];
   blogInfo: ArticleInfo = null;
+  userInfo: UserInfo = null;
   markdownURL: string = "";
 
   markdownReady(event: { ready: boolean, titles: Array<Title> }) {
     if (event.ready) {
-      this.loading = false;
       this.titleArray = event.titles;
-
+      setTimeout(() => {
+        this.loading = false;
+      }, 300)
       setTimeout(() => {
         hljs.highlightAll();
-      }, 200);
+      }, 500);
     }
   }
 
@@ -94,6 +100,22 @@ export class BlogDisplayComponent implements OnInit, OnChanges {
               this.blogInfo = value.result;
               this.markdownURL = value.result.Content;
               this.articleService.SetPreparedBlog(this.blogInfo);
+
+              // get article author public info
+              const usersID: Array<number> = [value.result.Uid]
+              this.userService.GetPublicUsersInfo(usersID).subscribe({
+                next: (val_userinfo) => {
+                  if (val_userinfo!==null){
+                    this.userInfo = val_userinfo.result[0];
+                  }else{
+                    this.userInfo.Username = "获取文章作者信息失败";  
+                  }
+                },
+                error: (err_userinfo) => {
+                  this.snackBarTips("获取文章作者信息失败");
+                  this.userInfo.Username = "获取文章作者信息失败";
+                }
+              })
             },
             error: (err) => {
               this.snackBarTips("文章不存在");
